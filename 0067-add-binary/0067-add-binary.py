5fec188fb239fe4bb10fc8887d2a9f1caf388ef9
ysooch0819 <59263935+ysooch0819@users.noreply.github.com>
2023-05-26 03:00:45 +0900

    Time: 12 ms (96.00%), Space: 13.4 MB (92.73%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return format(int(a, 2) + int(b, 2), 'b')
